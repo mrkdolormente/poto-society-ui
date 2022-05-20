c97c113fb0f10cba678c378d428f3da4f241c971
Mark Dolormente <markjosephandinodolormente@gmail.com>
2022-05-21 00:32:15 +0800

    Setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1A237E',
+        accent: '#B71C1C',
+        warn: '#FFFF8D',
+      },
+    },
+  },
+  plugins: [],
+};
